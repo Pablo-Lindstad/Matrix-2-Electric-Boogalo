5d6e3bb47fde4b756308b0262019d120f6719582
Pablo-Lindstad <pablobarreralindstad@gmail.com>
2022-05-23 14:39:14 +0200

    .

--- Maze.cpp
+++ Maze.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 
-// main() is where program execution begins.
 int main() {
-   std::cout << "Hello World"; // prints Hello World
+   std::cout << "Hello World"; 
    return 0;
 }
